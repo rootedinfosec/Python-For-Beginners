35dd93456ad0463a6b691c34a0cd0b07be83ffe9
Kenneth Wells <79234933+rootedinfosec@users.noreply.github.com>
2021-02-19 16:08:33 -0600

    Add files via upload
    This exercise goes over basic built in functions we have used throughout the exercises and a couple new ones.

---
+++ Basic_Built_in_Functions.py
@@ -0,0 +1,48 @@
+#This exercise will go over basic built in functions in Python. 
+
+#__________________________________________________________________#
+
+#Ex. 1 This example will go over the Print function. 
+
+print("Hello this function will print characters.")
+    #The print function is used to print onto the screen the results of the declared function. 
+
+#__________________________________________________________________#
+
+#Ex. 2 This example will go over the String function.
+print(str(5))
+    #The str function will turn what is inside the () into a string variable. The example shows the conversion of a interger variable to a string variable. 
+
+#___________________________________________________________________#
+
+#Ex. 3 This example will go over the Float function
+print(float(5))
+    #The float variable will convert a interger into a decimal number. The example shows the conversion of 5 into the float 5.0. 
+
+#___________________________________________________________________#
+
+#Ex. 4 This example will go over the boolean function.
+
+print(bool("True"))
+    #The bool variable will convert a string into a boolean variable. In this example we converted the string True into the boolean variable of True. 
+
+#___________________________________________________________________#
+
+#Ex. 5 This example will go over the length function. 
+
+print(len ('True'))
+    #The len function will tell you how many characters are inside of what you're declaring. In this example True gave us the len value of 4 since there are four letters in the work True. 
+
+#___________________________________________________________________#
+
+#Ex. 6 These two examples will go over the sorted function. 
+
+print(sorted([2,4,65,81,12,37,100]))
+    #The sorted function will sort the numbers in ascending order (lowest to highest). 
+
+print(sorted(["A", "V", "B", "d", "a", "z"]))
+    #This example shows you that the sorted function works on muliple variable types including strings. For letters it will Ascend in the order of A-Z,a-z. 
+
+print(sorted(["A", "V", "B", "d", "a", "z", "1", "3.0", "2"]))
+    #This example shows you that the sorted function can combine multiple variables that are declared the same variable type. We can see that it sorts from 1-#####, 1.0-######.##, A-Z, a-z.
+    #All of the variables are declared as strings due to them being wrapped in " ". 
