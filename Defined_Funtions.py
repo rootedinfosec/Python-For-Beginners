5b8bd4d69000fa00eb48442e8ccdda352c5d812a
Kenneth Wells <79234933+rootedinfosec@users.noreply.github.com>
2021-02-19 16:33:12 -0600

    Add files via upload

---
+++ Defined_Funtions.py
@@ -0,0 +1,21 @@
+#This exercise will go over defined funtions. 
+
+def my_function():
+    
+    
+    print("This is my function!")
+    print("This is a second string.")
+
+
+my_function()
+
+#Explaination over what is going on above is below.
+#______________________________________________________________#
+
+
+#Define (def) is used to define a function. The function is named using snake_case. CamelCase is used when defining classes. This will be covered in another exercise.
+#The () is called an argument. Parameters will go inside of the argument. Instead of using curly brackets to begin the function python uses a colon : 
+#
+#Python determines the end of a function based on the indentation after the function is called. You will use 4 spaces to determine the top of the function. 
+#Once you are done with the function just start a new line with no indentation and it will recognize the function is closed. Python does not need a semicolon ";" like other programming 
+#languages to close the function.
